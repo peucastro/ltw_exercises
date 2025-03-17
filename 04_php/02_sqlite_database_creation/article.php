3de4e94c999fd0684afcd9a20932e10bb3aceb5b
Pedro Castro <falecompedroac@gmail.com>
2025-03-17 19:50:12 +0000

    cerated article page

--- 04_php/02_sqlite_database_creation/article.php
+++ 04_php/02_sqlite_database_creation/article.php
@@ -1,15 +1,15 @@
 <?php
 $db = new PDO('sqlite:news.db');
 
-$stmt = $db->prepare('SELECT news.*, users.*, COUNT(comments.id) AS comments
-    FROM news JOIN
-      users USING (username) LEFT JOIN
-      comments ON comments.news_id = news.id
-    GROUP BY news.id, users.username
-    ORDER BY published DESC');
-$stmt->execute();
-$articles = $stmt->fetchAll();
+$stmt = $db->prepare('SELECT * FROM news JOIN users USING (username) WHERE id = ?');
+$stmt->execute(array($_GET['id']));
+$article = $stmt->fetch();
+
+$stmt = $db->prepare('SELECT * FROM comments JOIN users USING (username) WHERE news_id = ?');
+$stmt->execute(array($_GET['id']));
+$comments = $stmt->fetchAll();
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en-US">
@@ -63,25 +63,47 @@ $articles = $stmt->fetchAll();
     </article>
   </aside>
   <section id="news">
-    <?php foreach ($articles as $article) { ?>
-      <article>
-        <header>
-          <h1><a href="article.php?id=<?= $article['id'] ?>"><?= $article['title'] ?></a></h1>
-        </header>
-        <img src="https://picsum.photos/600/300?business" alt="">
-        <p><?= $article['introduction'] ?></p>
-        <footer>
-          <span class="author"><?= $article['name'] ?></span>
-          <span class="tags">
-            <?php $tags = explode(',', $article['tags']); ?>
-            <?php foreach ($tags as $tag) { ?>
-              <a href="/"><?= '#' . $tag ?></a>
-            <?php } ?>
-          </span>
-          <span class="date"><?= $date = date('F j', $article['published']) ?></span>
-          <a class="comments" href="article.php?id=<?= $article['id'] ?>#comments"><?= $article['comments'] ?></a>
-      </article>
-    <?php } ?>
+    <article>
+      <header>
+        <h1><a href="article.php?id=<?= $article['id'] ?>"><?= $article['title'] ?></a></h1>
+      </header>
+      <img src="https://picsum.photos/600/300?business" alt="">
+      <p><?= $article['fulltext'] ?></p>
+      <section id="comments">
+        <h1><?= count($comments) ?> Comments</h1>
+        <?php foreach ($comments as $comment) { ?>
+          <article class="comment">
+            <span class="user"> <?= $comment['username'] ?></span>
+            <span class="date"><?= $date = date('F j', $comment['published']) ?></span>
+            <p><?= $comment['text'] ?></p>
+          </article>
+        <?php } ?>
+        <form>
+          <h2>Add your voice...</h2>
+          <label>Username
+            <input type="text" name="username">
+          </label>
+          <label>E-mail
+            <input type="email" name="email">
+          </label>
+          <label>Comment
+            <textarea name="comment"></textarea>
+          </label>
+          <button formaction="#" formmethod="post">Reply</button>
+        </form>
+      </section>
+      <footer>
+        <span class="author"><?= $article['name'] ?></span>
+        <span class="tags">
+          <?php $tags = explode(',', $article['tags']); ?>
+          <?php foreach ($tags as $tag) { ?>
+            <a href="/"><?= '#' . $tag ?></a>
+          <?php } ?>
+        </span>
+        <span class="date"><?= $date = date('F j', $article['published']) ?></span>
+        <a class="comments" href="article.php?id=<?= $article['id'] ?>#comments"><?= count($comments) ?></a>
+      </footer>
+    </article>
   </section>
   <footer>
     <p>&copy; Fake News, 2022</p>
