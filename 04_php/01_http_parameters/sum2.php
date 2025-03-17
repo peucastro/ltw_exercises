087c3ca54ab5a7dc37d27aafaa5a370ead9dc934
Pedro Castro <falecompedroac@gmail.com>
2025-03-17 16:46:57 +0000

    php: corrected php

--- 04_php/01_http_parameters/sum2.php
+++ 04_php/01_http_parameters/sum2.php
@@ -2,9 +2,21 @@
 $num1 = $_GET['num1'];
 $num2 = $_GET['num2'];
 
-echo ($num1 + $num2);
+$sum = $num1 + $num2;
 ?>
 
-<br />
+<!DOCTYPE html>
+<html lang="en">
 
-<a href="form2.html"><?php echo 'Do another sum.' ?></a>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p><?php echo "$num1 + $num2 = $sum" ?></p>
+    <a href="form2.html">Do another sum.</a>
+</body>
+
+</html>
