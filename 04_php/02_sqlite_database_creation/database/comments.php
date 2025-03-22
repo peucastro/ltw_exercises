6ec31a8cf01c84c98427fd6d6fb68b0671816245
Pedro Castro <falecompedroac@gmail.com>
2025-03-22 23:14:33 +0000

    formatting and fixed db path

--- 04_php/02_sqlite_database_creation/database/comments.php
+++ 04_php/02_sqlite_database_creation/database/comments.php
@@ -4,6 +4,5 @@ function getComments($db, $id)
     $stmt = $db->prepare('SELECT * FROM comments JOIN users USING (username) WHERE news_id = :id');
     $stmt->bindParam(':id', $id);
     $stmt->execute();
-    $comments = $stmt->fetchAll();
-    return $comments;
+    return $stmt->fetchAll();
 }
--- 04_php/02_sqlite_database_creation/database/connection.php
+++ 04_php/02_sqlite_database_creation/database/connection.php
@@ -1,6 +1,5 @@
 <?php
 function getDatabaseConnection()
 {
-    $db = new PDO('sqlite:news.db');
-    return $db;
+    return new PDO('sqlite:database/news.db');
 }
