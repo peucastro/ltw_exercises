b9c76e1c2631c128d48d124ae49351f7b7491162
Pedro Castro <falecompedroac@gmail.com>
2025-03-24 17:32:27 +0000

    moved css to separate folder

--- 04_php/02_sqlite_database_creation/templates/common.php
+++ 04_php/02_sqlite_database_creation/templates/common.php
@@ -8,11 +8,11 @@
         <title><?= $title ?></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="style.css" rel="stylesheet">
-        <link href="layout.css" rel="stylesheet">
-        <link href="responsive.css" rel="stylesheet">
-        <link href="comments.css" rel="stylesheet">
-        <link href="forms.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/layout.css" rel="stylesheet">
+        <link href="css/responsive.css" rel="stylesheet">
+        <link href="css/comments.css" rel="stylesheet">
+        <link href="css/forms.css" rel="stylesheet">
     </head>
 
     <body>
