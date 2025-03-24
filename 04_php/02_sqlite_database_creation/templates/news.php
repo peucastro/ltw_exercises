e8ed6ef0225dd053cf659392394f44330fcd681a
Pedro Castro <falecompedroac@gmail.com>
2025-03-24 17:02:31 +0000

    corrected article text

--- 04_php/02_sqlite_database_creation/templates/news.php
+++ 04_php/02_sqlite_database_creation/templates/news.php
@@ -5,6 +5,28 @@
             <h1><a href="article.php?id=<?= $article['id'] ?>"><?= $article['title'] ?></a></h1>
         </header>
         <img src="https://picsum.photos/600/300?business" alt="">
+        <p><?= $article['introduction'] ?></p>
+        <footer>
+            <span class="author"><?= $article['name'] ?></span>
+            <span class="tags">
+                <?php $tags = explode(',', $article['tags']); ?>
+                <?php foreach ($tags as $tag) { ?>
+                    <a href="/"><?= '#' . $tag ?></a>
+                <?php } ?>
+            </span>
+            <span class="date"><?= $date = date('F j', $article['published']) ?></span>
+            <a class="comments" href="article.php?id=<?= $article['id'] ?>#comments"><?= $article['comments'] ?></a>
+        </footer>
+    </article>
+<?php } ?>
+
+<?php function output_full_article($article)
+{ ?>
+    <article>
+        <header>
+            <h1><a href="article.php?id=<?= $article['id'] ?>"><?= $article['title'] ?></a></h1>
+        </header>
+        <img src="https://picsum.photos/600/300?business" alt="">
         <p><?= $article['fulltext'] ?></p>
     </article>
 <?php } ?>
