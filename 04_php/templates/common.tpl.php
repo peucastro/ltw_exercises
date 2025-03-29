04cc0509590301635edc49dc233810b1c12cc0f1
Pedro Castro <falecompedroac@gmail.com>
2025-03-29 15:22:14 +0000

    pages folder

--- 04_php/templates/common.tpl.php
+++ 04_php/templates/common.tpl.php
@@ -13,11 +13,11 @@ declare(strict_types=1);
         <title><?= $title ?></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/style.css" rel="stylesheet">
-        <link href="css/layout.css" rel="stylesheet">
-        <link href="css/responsive.css" rel="stylesheet">
-        <link href="css/comments.css" rel="stylesheet">
-        <link href="css/forms.css" rel="stylesheet">
+        <link href="../css/style.css" rel="stylesheet">
+        <link href="../css/layout.css" rel="stylesheet">
+        <link href="../css/responsive.css" rel="stylesheet">
+        <link href="../css/comments.css" rel="stylesheet">
+        <link href="../css/forms.css" rel="stylesheet">
     </head>
 
     <body>
